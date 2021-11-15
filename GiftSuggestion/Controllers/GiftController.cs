49ec67666f7e21fa42e0a5c38794171027193b5c
hanielsouza <hanielsouza@hotmail.com>
2021-11-15 14:04:24 -0300

    Adicionado controler para fazer enviar dados para o método gravar da bussiness

--- GiftSuggestion/Controllers/GiftController.cs
+++ GiftSuggestion/Controllers/GiftController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using GiftSuggestion.Models;
+using GiftSuggestion.Business;
+using System.Threading.Tasks;
 
 namespace GiftSuggestion.Controllers
 {
@@ -8,18 +10,21 @@ namespace GiftSuggestion.Controllers
     [Route("[controller]")]
     public class GiftController : ControllerBase
     {
-        
+        private readonly IGiftBusiness _giftBusiness;
         private readonly ILogger<GiftController> _logger;
 
-        public GiftController(ILogger<GiftController> logger)
+        public GiftController(IGiftBusiness giftBusiness, ILogger<GiftController> logger)
         {
+            _giftBusiness = giftBusiness;
             _logger = logger;
         }
 
         [HttpPost]
-        public ActionResult Post()
+        [Route("cadastrar")]
+        public async Task<IActionResult> Post([FromBody] GiftModel model)
         {
-            return Ok("produto tal cadastrado com sucesso");
+            var giftCadastrado = await _giftBusiness.Cadastrar(model);
+            return Ok(giftCadastrado); // adicionar uma mensagem indicando que o nome foi cadastrado
         }
     }
 }
